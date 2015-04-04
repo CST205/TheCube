b4938bda54a572c3ee07710af8996613bdc8db4f
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2015-04-04 19:33:14 -0400

    renderer setup

--- public/index.php
+++ public/index.php
@@ -1,1 +1,18 @@
-First commit
+<html>
+	<head>
+		<title>The Sound Cube</title>
+		<style>
+			*{
+				padding:0px;
+				margin:0px;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="container"></div>
+		<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r71/three.min.js"></script>
+		<script src="/stats.js"></script>
+		<script src="/visual.js"></script>
+	</body>
+</html>
---
+++ public/visual.js
@@ -0,0 +1,34 @@
+var container, stats;
+var camera, scene, renderer;
+var cube;
+function init() {
+	$("#container").css({"width":$(document).width()+"px","height":$(document).height()+"px"});
+	container = document.getElementById( 'container' );
+	camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 1, 3500 );
+	camera.position.z = 2750;
+	scene = new THREE.Scene();
+	scene.add( new THREE.AmbientLight( 0x444444 ) );
+	var light1 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+	light1.position.set( 1, 1, 1 );
+	scene.add( light1 );
+	renderer = new THREE.WebGLRenderer( { antialias: false } );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	container.appendChild( renderer.domElement );
+	stats = new Stats();
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.top = '0px';
+	container.appendChild( stats.domElement );
+}
+function animate() {
+	requestAnimationFrame( animate );
+	render();
+	stats.update();
+}
+function render() {
+	var bpm = 120;
+
+	renderer.render( scene, camera );
+}
+init();
+animate();
