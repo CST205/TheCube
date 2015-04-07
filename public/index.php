79460ac5987c6b4368284389398db525ad263963
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2015-04-07 16:16:23 -0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@
 			}
 		</style>
 	</head>
-	<body>Meow
+	<body>
 		<div id="container"></div>
 		<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r71/three.min.js"></script>
