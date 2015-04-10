0387e0cde83b7cdd25edf6fcca848385053526da
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2015-04-09 21:16:49 -0400

    cube is working

--- public/cube.js
+++ public/cube.js
@@ -2,13 +2,25 @@ function Cube(size,x,y,z){
 	this.size=size;
 	this.angle=0;
 	this.padding = 100;
+	this.mesh;
 	this.cubeObject;
 	this.voxel = new Array();
+	/*for(var ix = 0;ix < (size+this.padding);ix++){
+		if(this.voxel[ix]== undefined)
+			this.voxel[ix]=new Array();
+		for(var iy = 0;iy < (size+this.padding);iy++){
+			if(this.voxel[ix][iy]== undefined)
+				this.voxel[ix][iy]=new Array();
+			for(var iz = 0;iz < (size+this.padding);iz++){
+				this.voxel[ix][iy][iz] = 0;
+			}
+		}
+	}*/
 	for(var ix = this.padding-1;ix < (size+this.padding);ix++){
-		if(!this.voxel[ix])
+		if(this.voxel[ix]== undefined)
 			this.voxel[ix]=new Array();
 		for(var iy = this.padding;iy < (size+this.padding);iy++){
-			if(!this.voxel[ix][iy])
+			if(this.voxel[ix][iy]== undefined)
 				this.voxel[ix][iy]=new Array();
 			for(var iz = this.padding;iz < (size+this.padding);iz++){
 				this.voxel[ix][iy][iz] = 1;
@@ -31,88 +43,98 @@ function CubeRender(){
 	
 }
 function checkExists(vox,x,y,z){
-	if(vox[x])
-		if(vox[x][y])
-			if(vox[x][y][z])
-				return ((vox[x][y][z]!=1)?false:true);
-	return false;
+	if(vox[x]!=undefined)
+		if(vox[x][y]!=undefined)
+			if(vox[x][y][z]!=undefined)
+				return ((vox[x][y][z]!=1)?true:false);
+	return true;
 }
 function adjacentCheck(vox,x,y,z){
-	var left = checkExists(x-1,y,z);
-	var right = checkExists(x+1,y,z);
-	var top = checkExists(x,y-1,z);
-	var bottom = checkExists(x,y+1,z);
-	var front = checkExists(x,y,z-1);
-	var back = checkExists(x,y,z+1);
+	var left = checkExists(vox,x-1,y,z);
+	var right = checkExists(vox,x+1,y,z);
+	var top = checkExists(vox,x,y-1,z);
+	var bottom = checkExists(vox,x,y+1,z);
+	var front = checkExists(vox,x,y,z-1);
+	var back = checkExists(vox,x,y,z+1);
 	return [left,right,top,bottom,front,back];
 }
 function CubeUpdate(){
-	this.cubeObject = new THREE.Geometry();
-	for(var ix = 0;ix < (size+this.padding);ix++){
-		if(this.voxel[ix]){
-			for(var iy = 0;iy < (size+this.padding);iy++){
-				if(this.voxel[ix][iy]){
-					for(var iz = 0;iz < (size+this.padding);iz++){
-						if(this.voxel[ix][iy][iz]){
-							var geometry = new THREE.Geometry();
+	var faceIndex = 0;
+	var geometry = new THREE.Geometry();
+	for(var ix = 0;ix < (this.size+this.padding);ix++){
+		if(this.voxel[ix]!= undefined){
+			for(var iy = 0;iy < (this.size+this.padding);iy++){
+				if(this.voxel[ix][iy]!= undefined){
+					for(var iz = 0;iz < (this.size+this.padding);iz++){
+						if(this.voxel[ix][iy][iz]!= undefined){
 							ac = adjacentCheck(this.voxel,ix,iy,iz);
-							if(ac[0]){ // left
-								geometry.vertices.push(
-									new THREE.Vector3((ix*10)-10,(iy*10), (iz*10) ),
-									new THREE.Vector3((ix*10)-10,(iy*10)-10, (iz*10) ),
-									new THREE.Vector3((ix*10)-10,(iy*10)-10, (iz*10)-10 ),
-									new THREE.Vector3((ix*10)-10,(iy*10), (iz*10)-10 )
-								);
-							}
-							this.cubeObject.merge(geometry);
-							geometry = new THREE.Geometry();
-							if(ac[1]){ // Right
-								geometry.vertices.push(
-									new THREE.Vector3((ix*10),(iy*10), (iz*10)-10 ),
-									new THREE.Vector3((ix*10),(iy*10)-10, (iz*10)-10 ),
-									new THREE.Vector3((ix*10),(iy*10)-10, (iz*10) ),
-									new THREE.Vector3((ix*10),(iy*10), (iz*10) )
-								);
-							}
-							this.cubeObject.merge(geometry);
-							geometry = new THREE.Geometry();
-							if(ac[2]){ // Top
-								geometry.vertices.push(
-									new THREE.Vector3((ix*10),(iy*10), (iz*10)-10 ),
-									new THREE.Vector3((ix*10),(iy*10), (iz*10) ),
-									new THREE.Vector3((ix*10)-10,(iy*10), (iz*10) ),
-									new THREE.Vector3((ix*10)-10,(iy*10), (iz*10)-10 )
-								);
-							}
-							this.cubeObject.merge(geometry);
-							geometry = new THREE.Geometry();
-							if(ac[3]){ // Bottom
-								geometry.vertices.push(
-									new THREE.Vector3((ix*10),(iy*10)-10, (iz*10)-10 ),
-									new THREE.Vector3((ix*10),(iy*10)-10, (iz*10) ),
-									new THREE.Vector3((ix*10)-10,(iy*10)-10, (iz*10) ),
-									new THREE.Vector3((ix*10)-10,(iy*10)-10, (iz*10)-10 )
-								);
-							}
-							this.cubeObject.merge(geometry);
-							geometry = new THREE.Geometry();
-							if(ac[4]){ // Front
-								geometry.vertices.push(
-									new THREE.Vector3((ix*10)-10,(iy*10), (iz*10)-10 ),
-									new THREE.Vector3((ix*10)-10,(iy*10)-10, (iz*10)-10 ),
-									new THREE.Vector3((ix*10),(iy*10)-10, (iz*10)-10 ),
-									new THREE.Vector3((ix*10),(iy*10), (iz*10)-10 )
-								);
-							}
-							this.cubeObject.merge(geometry);
-							geometry = new THREE.Geometry();
-							if(ac[5]){ // Back
-								geometry.vertices.push(
-									new THREE.Vector3((ix*10)-10,(iy*10), (iz*10) ),
-									new THREE.Vector3((ix*10)-10,(iy*10)-10, (iz*10) ),
-									new THREE.Vector3((ix*10),(iy*10)-10, (iz*10)-10 ),
-									new THREE.Vector3((ix*10),(iy*10), (iz*10) )
-								);
+							if(this.voxel[ix][iy][iz]==1){
+								if(ac[0]){ // left
+									geometry.vertices.push(
+										new THREE.Vector3((ix*10)-10,(iy*10), (iz*10) ),
+										new THREE.Vector3((ix*10)-10,(iy*10)-10, (iz*10) ),
+										new THREE.Vector3((ix*10)-10,(iy*10)-10, (iz*10)-10 ),
+										new THREE.Vector3((ix*10)-10,(iy*10), (iz*10)-10 )
+									);
+									geometry.faces.push(new THREE.Face3( faceIndex, faceIndex+1, faceIndex+2, 1, new THREE.Color( 0xffff0f ) ));
+									geometry.faces.push(new THREE.Face3( faceIndex, faceIndex+2, faceIndex+3, 1, new THREE.Color( 0xffff0f ) ));
+									faceIndex +=4; 
+								}
+								if(ac[1]){ // Right
+									geometry.vertices.push(
+										new THREE.Vector3((ix*10),(iy*10), (iz*10)-10 ),
+										new THREE.Vector3((ix*10),(iy*10)-10, (iz*10)-10 ),
+										new THREE.Vector3((ix*10),(iy*10)-10, (iz*10) ),
+										new THREE.Vector3((ix*10),(iy*10), (iz*10) )
+									);
+									geometry.faces.push(new THREE.Face3( faceIndex, faceIndex+1, faceIndex+2, 1, new THREE.Color( 0xff0fff ) ));
+									geometry.faces.push(new THREE.Face3( faceIndex, faceIndex+2, faceIndex+3, 1, new THREE.Color( 0xff0fff ) ));
+									faceIndex +=4; 
+								}
+								if(ac[3]){ // Top
+									geometry.vertices.push(
+										new THREE.Vector3((ix*10),(iy*10), (iz*10)-10 ),
+										new THREE.Vector3((ix*10),(iy*10), (iz*10) ),
+										new THREE.Vector3((ix*10)-10,(iy*10), (iz*10) ),
+										new THREE.Vector3((ix*10)-10,(iy*10), (iz*10)-10 )
+									);
+									geometry.faces.push(new THREE.Face3( faceIndex, faceIndex+1, faceIndex+2, 1, new THREE.Color( 0x0fff0f ) ));
+									geometry.faces.push(new THREE.Face3( faceIndex, faceIndex+2, faceIndex+3, 1, new THREE.Color( 0x0fff0f ) ));
+									faceIndex +=4; 
+								}
+								if(ac[2]){ // Bottom
+									geometry.vertices.push(
+										new THREE.Vector3((ix*10),(iy*10)-10, (iz*10)-10 ),
+										new THREE.Vector3((ix*10),(iy*10)-10, (iz*10) ),
+										new THREE.Vector3((ix*10)-10,(iy*10)-10, (iz*10) ),
+										new THREE.Vector3((ix*10)-10,(iy*10)-10, (iz*10)-10 )
+									);
+									geometry.faces.push(new THREE.Face3( faceIndex, faceIndex+1, faceIndex+2, 1, new THREE.Color( 0x00ffff ) ));
+									geometry.faces.push(new THREE.Face3( faceIndex, faceIndex+2, faceIndex+3, 1, new THREE.Color( 0x00ffff ) ));
+									faceIndex +=4; 
+								}
+								if(ac[4]){ // Front
+									geometry.vertices.push(
+										new THREE.Vector3((ix*10)-10,(iy*10), (iz*10)-10 ),
+										new THREE.Vector3((ix*10)-10,(iy*10)-10, (iz*10)-10 ),
+										new THREE.Vector3((ix*10),(iy*10)-10, (iz*10)-10 ),
+										new THREE.Vector3((ix*10),(iy*10), (iz*10)-10 )
+									);
+									geometry.faces.push(new THREE.Face3( faceIndex, faceIndex+1, faceIndex+2, 1, new THREE.Color( 0xff00ff ) ));
+									geometry.faces.push(new THREE.Face3( faceIndex, faceIndex+2, faceIndex+3, 1, new THREE.Color( 0xff00ff ) ));
+									faceIndex +=4; 
+								}
+								if(ac[5]){ // Back
+									geometry.vertices.push(
+										new THREE.Vector3((ix*10)-10,(iy*10), (iz*10) ),
+										new THREE.Vector3((ix*10)-10,(iy*10)-10, (iz*10) ),
+										new THREE.Vector3((ix*10),(iy*10)-10, (iz*10) ),
+										new THREE.Vector3((ix*10),(iy*10), (iz*10) )
+									);
+									geometry.faces.push(new THREE.Face3( faceIndex, faceIndex+1, faceIndex+2, 1, 0xffff00 ));
+									geometry.faces.push(new THREE.Face3( faceIndex, faceIndex+2, faceIndex+3, 1, 0xffff00 ));
+									faceIndex +=4; 
+								}
 							}
 						}	
 					}
@@ -120,5 +142,13 @@ function CubeUpdate(){
 			}
 		}
 	}
-	scene.add(this.cubeObject);
+	var material = new THREE.MeshLambertMaterial( { color:0xffffff, shading: THREE.FlatShading,vertexColors: THREE.VertexColors  } );
+	mesh = new THREE.Mesh( geometry, material );
+	mesh.position.x=this.x;
+	mesh.position.y=this.y;
+	mesh.position.z=this.z;
+	scene.add(mesh);
+	console.log(mesh);
+	camera.lookAt(mesh);
+    	camera.updateProjectionMatrix();
 }
--- public/visual.js
+++ public/visual.js
@@ -4,29 +4,39 @@ var cube=new Cube(20,0,0,0);
 function init() {
 	$("#container").css({"width":$(document).width()+"px","height":$(document).height()+"px"});
 	container = document.getElementById( 'container' );
-	camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 1, 3500 );
-		camera.position.z = 2750;
+	/*camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 1, 3500 );
+		camera.position.z = 1000;
+		camera.position.x = 1000;
+		camera.position.y = 500;
+	*/
+	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 3500 );
+		camera.position.z = 60;
+		camera.position.x = 100;
+		camera.position.y = 100;
+		controls = new THREE.OrbitControls( camera );
+		controls.addEventListener( 'change', render );
 
 	scene = new THREE.Scene();
-		scene.add( new THREE.AmbientLight( 0x444444 ) );
+		scene.add( new THREE.AmbientLight( 0xeeeeee ) );
 
-	var light1 = new THREE.DirectionalLight( 0xffffff, 0.5 );
-		light1.position.set( 1, 1, 1 );
-		scene.add( light1 );
-
+	
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 		renderer.setPixelRatio( window.devicePixelRatio );
 		renderer.setSize( window.innerWidth, window.innerHeight );
+		renderer.setClearColor( 0xffffff );
 		container.appendChild( renderer.domElement );
-
+	
 	stats = new Stats();
 		stats.domElement.style.position = 'absolute';
 		stats.domElement.style.top = '0px';
 		container.appendChild( stats.domElement );
+	cube.update();
 }
 function animate() {
 	requestAnimationFrame( animate );
 	render();
+	controls.update();
+	//console.log(camera.position);
 	stats.update();
 }
 function render() {
