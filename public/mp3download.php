1689db4a976a7204a323993e5df8be9622323f87
BuildTools <nathaniel.davidson@gmail.com>
2015-04-23 17:02:25 -0700

    assda

--- public/mp3download.php
+++ public/mp3download.php
@@ -1,5 +1,5 @@
 <?
-$url = "https://www.youtube.com/watch?v=Ao8cGLIMtvg";
+$url = base64_decode($_GET['song']);
 $file = "song";
 system("youtube-dl ".$url." -x -o \"".$file.".%(ext)s\"");
 system("ffmpeg -i ".$file.".m4a ".$file.".mp3");
